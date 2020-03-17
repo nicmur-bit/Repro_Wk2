b497d93b1367ec46532e16d932db3d3b661b0963
Murphy <mur324@csiro.au>
2020-03-17 10:10:07 +1000

    First exercise, seaparate min max

---
+++ Reproducibility_R.R
@@ -0,0 +1,10 @@
+install.packages("tidyverse")
+BOMdata <- csv_read("data/BOM_data.csv")
+library(tidyverse)
+BOMdata <- read_csv("data/BOM_data.csv")
+BOMdata <- read_csv("data/BOM_stations.csv")
+
+#For BOM_data, how many days have a minimum temperature?
+#maximum temperature? and rainfall measurement?
+
+

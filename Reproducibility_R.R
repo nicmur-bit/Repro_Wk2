8f48bb0b2bf3610da18a07ad7a2aa3aad3a8ec2e
Murphy <mur324@csiro.au>
2020-03-17 14:41:34 +1000

    question 2 with lots of help

--- Reproducibility_R.R
+++ Reproducibility_R.R
@@ -4,7 +4,7 @@ library(tidyverse)
 BOMdata <- read_csv("data/BOM_data.csv")
 BOMstations <- read_csv("data/BOM_stations.csv")
 
-#For BOM_data, how many days have a minimum temperature?
+#For each startion(BOM_data), how many days have a minimum temperature?
 #maximum temperature? and rainfall measurement?
 
 view(BOMdata)
@@ -15,7 +15,22 @@ BOM_levels <- BOMdata %>%
   group_by(Station_number) %>% 
   summarise(n())
 
-view(BOM_levels)
+BOM_levels_result <- BOM_levels
 
+#Which month saw the lowest, average, daily, temperature difference
 
+BOM_months <- BOMdata %>%  
+  separate(Temp_min_max, into = c('temp_min', 'temp_max'), sep = "/")  %>% 
+  filter(temp_min != '-', temp_max != '-')
   
+BOM_months$temp_min <- as.numeric(BOM_months$temp_min)  
+BOM_months$temp_max <- as.numeric(BOM_months$temp_max)
+
+BOM_month_mutate <- mutate(BOM_months, temp_diff = temp_max - temp_min) 
+
+  group_by(BOM_month_mutate, Month) %>% 
+  summarise(mean_temp_diff = mean(temp_diff)) %>% 
+    arrange(mean_temp_diff)
+  
+
+

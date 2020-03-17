89295e1316dbcc61e9b5567b64c995df017fa400
Murphy <mur324@csiro.au>
2020-03-17 11:55:58 +1000

    updated code

--- Reproducibility_R.R
+++ Reproducibility_R.R
@@ -2,9 +2,17 @@ install.packages("tidyverse")
 BOMdata <- csv_read("data/BOM_data.csv")
 library(tidyverse)
 BOMdata <- read_csv("data/BOM_data.csv")
-BOMdata <- read_csv("data/BOM_stations.csv")
+BOMstations <- read_csv("data/BOM_stations.csv")
 
 #For BOM_data, how many days have a minimum temperature?
 #maximum temperature? and rainfall measurement?
 
+view(BOMdata)
 
+BOM_levels <- BOMdata %>%  
+  separate(Temp_min_max, into = c('temp_min', 'temp_max'), sep = "/") %>%  
+  filter(temp_min != '-', temp_max != '-', Rainfall != '-') %>% 
+  group_by(temp_min, temp_max, Rainfall) 
+  
+ 
+  

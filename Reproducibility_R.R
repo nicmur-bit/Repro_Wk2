843aa2d42594e811e9b771a24b8c1412c40e20cc
Murphy <mur324@csiro.au>
2020-03-18 13:27:06 +1000

    Thank the universe for Valentino, question 3 and question 4 complete

--- Reproducibility_R.R
+++ Reproducibility_R.R
@@ -1,9 +1,10 @@
 library(tidyverse)
+getwd()
 BOMdata <- read_csv("data/BOM_data.csv")
 BOMstations <- read_csv("data/BOM_stations.csv")
 
 #Question 1
-#For each startion(BOM_data), how many days have a minimum temperature?
+#For each station(BOM_data), how many days have a minimum temperature?
 #maximum temperature? and rainfall measurement?
 
 BOM_levels <- BOMdata %>%  
@@ -16,12 +17,11 @@ BOM_levels_result <- BOM_levels
 
 #Question 1 answer is BOM_levels_result, table output
 
-
 #Question 2
 #Which month saw the lowest, average, daily, temperature difference
 
 BOM_months <- BOMdata %>%  
-  separate(Temp_min_max, into = c('temp_min', 'temp_max'), sep = "/")  %>% 
+  separate(Temp_min_max, into = c('temp_min', 'temp_max'), sep = "/") %>% 
   filter(temp_min != '-', temp_max != '-')
   
 BOM_months$temp_min <- as.numeric(BOM_months$temp_min)  
@@ -37,7 +37,33 @@ BOM_month_mutate <- mutate(BOM_months, temp_diff = temp_max - temp_min)
 
 #Question3
 #which state saw the lowest average daily temp difference
+
+station_long <- gather(BOMstations, key = 'Station_number', value = 'misc' , -info)
   
- #tip is to spread and then gather data or vice versa 
+station_spread <- spread(station_long, key = 'info' , value = 'misc')
 
+station_tidy <- mutate(station_spread, Station_number = as.numeric(Station_number))
 
+BOM_levels <- BOMdata %>%  
+  separate(Temp_min_max, into = c('temp_min', 'temp_max'), sep = "/")
+
+BOM_combined <- left_join(BOM_levels, station_tidy)
+
+BOM_final <- BOM_combined %>% 
+  mutate(temp_diff = as.numeric(temp_max) - as.numeric(temp_min)) %>% 
+  filter(!is.na(temp_diff)) %>% 
+  group_by(state) %>% 
+  summarise(average_temp = mean(temp_diff)) %>% 
+  arrange(average_temp)
+
+#which station has the lowest & hightest longitude, average solar exposure
+#answer to question4
+
+Q_four_answer <- BOM_combined %>% 
+  mutate(Solar_exposure = as.numeric(Solar_exposure), lon = as.numeric(lon)) %>% 
+  filter(!is.na(Solar_exposure)) %>% 
+  filter(lon == max(lon) | lon == min(lon)) %>%
+  group_by(Station_number, lon) %>% 
+  summarise(average_solar_expousure = mean(Solar_exposure))
+
+Q_four_answer
